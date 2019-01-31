62065feb69fbcc776a85bd264d35af6c9d02144f
emiharbur <928728260@qq.com>
2019-01-31 10:33:12 +0800

    modelworking1.0

--- sqlweb/models.py
+++ sqlweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class StaffBaseInfo(models.Model):
+    stuffname = models.CharField(max_length=20,primary_key=True)
+    stuffstate = models.BooleanField(default=True)
+    stuffaccount = models.CharField(max_length=20)
+    stuffphonenumber = models.IntegerField(max_length=20)
+    stuffremark = models.TextField()
+
 # Create your models here.
